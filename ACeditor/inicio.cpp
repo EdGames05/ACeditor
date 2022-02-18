cbae2e7dbd8f96ff9f3f6070e67ec4bdf897d0c1
Allam05 <32823797+Allam05@users.noreply.github.com>
2022-02-18 15:23:29 -0600

    Añadiendo busqueda por char

--- ACeditor/inicio.cpp
+++ ACeditor/inicio.cpp
@@ -1,9 +1,12 @@
 #include "ACadena.h"
 #include <stdio.h>
+#include <locale.h>
 
 int main(int argc, char** argv) {
+	// Poner todos los char en modo español
+	setlocale(LC_ALL, "es_ES");
 
 	ACadena str = "Algo de la cádena \n";
-	printf(str.c_str());
+	printf("%s \n", str[1].c_str());
 	return 0;
 }
